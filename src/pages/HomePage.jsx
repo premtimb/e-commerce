4d77a6c3765d3899c35f9917bd306e3cb4caa8e4
Premtim <premtim.berisha@hotmail.com>
2025-12-09 18:04:17 +0100

    Lift card up and share it between the pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,18 +4,13 @@ import { Header } from '../components/Header';
 import CheckmarkIcon from '../assets/images/checkmark.png';
 import './HomePage.css';
 
-export function HomePage() {
+export function HomePage({ cart }) {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data);
-            });
-        axios.get('/api/cart-items')
-            .then((response) => {
-                setCart(response.data);
             });
     },[]);
 
