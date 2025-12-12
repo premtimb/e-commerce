99c23fe43cdabec90a1b9a1a698a841ee1757e77
Premtim <premtim.berisha@hotmail.com>
2025-12-12 21:26:06 +0100

    Check if it work with 0 and negative formatMoney

--- src/utils/money.js
+++ src/utils/money.js
@@ -1,3 +1,7 @@
 export function formatMoney(amountCents) {
-    return `$${(amountCents / 100).toFixed(2)}`;
+    if(amountCents < 0) {
+        return `-$${(Math.abs(amountCents) / 100).toFixed(2)}`;
+    }
+
+        return `$${(amountCents / 100).toFixed(2)}`;
 }
